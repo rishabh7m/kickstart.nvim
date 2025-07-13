e915ec4ee8ba28a3ca1e3e3b251e8caee6cae96f
rishabh7m <rishabh.makhija07@gmail.com>
2025-07-13 12:00:52 +0530

    Neo-Tree: Disable by default

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -10,18 +10,27 @@ return {
     require("neo-tree").setup({
       filesystem = {
         follow_current_file = true,
-        hijack_netrw_behavior = "open_default",
+        hijack_netrw_behavior = "disabled",
       },
+      window = {
+        open_in_tab = true, -- Open files in a new tab
+      },
+      source_selector = {
+      winbar = true, -- Show source selector in the window bar
+      statusline = false,
+      show_scrolled_off_parent_node = true,
+    },
     })
 
     -- Key mapping to toggle Neo-tree
-    vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", { desc = "Toggle File Explorer" })
+    vim.keymap.set("n", "<leader>p", ":Neotree toggle<CR>", { desc = "Toggle File Explorer" })
+    vim.keymap.set("n", "<leader>pf", ":Neotree focus<CR>", { desc = "Focus Neo-tree" })
 
     -- Optional: open Neo-tree automatically on startup
-    vim.api.nvim_create_autocmd("VimEnter", {
-      callback = function()
-        require("neo-tree.command").execute({ action = "show" })
-      end,
-    })
+    -- vim.api.nvim_create_autocmd("VimEnter", {
+    --   callback = function()
+    --     require("neo-tree.command").execute({ action = "show" })
+    --   end,
+    -- })
   end
 }
